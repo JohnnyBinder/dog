3cfa7d5509e0b497ac9bd124cdae76e76b9e368d
Jon Binder <binder11@uwindsor.ca>
2023-05-22 10:10:44 -0400

    v1.2.0

--- dog.go
+++ dog.go
@@ -9,3 +9,7 @@ func WhenGrownUp(s string) string {
 func WhenBigAndGrownUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s) + "!!!"
 }
+
+func Tag12() string {
+	return "This is version v1.2.0 of dog."
+}
