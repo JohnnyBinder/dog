0fb196ed823cbb9912df7d40c5bfb1507751282e
Jon Binder <binder11@uwindsor.ca>
2023-05-22 09:48:08 -0400

    Biggest bark

--- dog.go
+++ dog.go
@@ -5,3 +5,7 @@ import "strings"
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
+
+func WhenBigAndGrownUp(s string) string {
+	return "When the puppy grows up it says: " + strings.ToUpper(s) + "!!!"
+}
